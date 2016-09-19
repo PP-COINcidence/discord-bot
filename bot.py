a5fe8ed27a3e110bbb4a320aaf49b611c71f7365
Mohammed Hossain <mhossain@itbit.com>
2016-09-19 11:50:10 -0400

    Adds gitignore and initial bot.

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import json
+import random
+
+description = '''A really ghetto Discord bot.'''
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command()
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+with open('config.json') as f:
+    config = json.load(f)
+
+bot.run(config['token'])
